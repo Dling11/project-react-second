e532dfb480f59ffbf2bafc26e4981fee32350af6
Rowell Jay Rodriguez <rodriguez.rowelljay@gmail.com>
2023-02-22 14:20:32 +0800

    react meme generator

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav className="myNav">
+            <img src="../img/TrollFace.png" alt="TrollFace" className="meme-logo" />
+            <span className="meme-title">Meme Generator</span>
+            <span className="meme-title-right">React Course - Project 3</span>
+        </nav>
+    )
+}
+
+export default Nav
