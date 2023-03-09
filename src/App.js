8f20c4a89943d8ff6f3501a690cd007d24749054
Rowell Jay Rodriguez <rodriguez.rowelljay@gmail.com>
2023-03-09 14:42:38 +0800

    Portfolio animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import Header from "./components/Header"
-import Meme from "./components/Meme"
+import "./App.scss";
+import { Routes, Route } from 'react-router-dom';
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="myApp">
-      <Header />
-      <Meme />
-    </div>
+    <>
+      <Routes>
+
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+        
+      </Routes>
+    </>
   )
 }
+
 export default App
+
+//contact form ===> npm i @emailjs/browser
+
+//==============>  npm i @fortawesome/free-brands-svg-icons
+
+//==== Navigation ========> npm i @fortawesome/free-solid-svg-icons
+
+//==================> npm i @fortawesome/react-fontawesome
+
+//======== Animation ========> npm i animate.css
+
+//==========> Advance animation ==> npm i gsap-trial
+
+//=======Loaders Packman loader ====> npm i loaders.css
+
+//====About map==> npm i react-leaflet
+
+//==========> npm i react-loaders
+
+//=========> using Saas ========> npm i sass
